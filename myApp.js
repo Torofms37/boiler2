c368a41c72012736d8ddf6d1f91d8e732476937e
torofms37 <torofms37@gmail.com>
2025-06-11 12:00:31 -0700

    aa

--- myApp.js
+++ myApp.js
@@ -4,7 +4,18 @@ let app = express();
 console.log("Hello World");
 
 app.get('/', (req, res) => {
-  res.send('Hello Express');
+  res.sendFile(__dirname + '/views/index.html');
 });
+
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/json', (req, res) => {
+  res.json({ "message": "Hello json" });
+  if(process.env.MESSAGE_STYLE === 'uppercase') {
+    message = message.toUpperCase();
+  }
+  res.json({ "message": message });
+});
+
 
 module.exports = app;
